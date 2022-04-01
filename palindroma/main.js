27488e2f503c7eb61c51ed7e8f312afe3630e551
NicolaMigliore <auster.mig@gmail.com>
2022-04-01 15:21:14 +0200

    add comments in palindroma js

---
+++ palindroma/main.js
@@ -0,0 +1,20 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma */
+
+/* Definizione
+Il palindromo è una sequenza di lettere o di sillabe che possa essere letta anche in senso contrario dando come esito o la sequenza di partenza o un’altra sequenza pure dotata di senso. (es. otto, osso, level )*/
+
+
+//Chiedo all'untente di inserire una parola
+let user_word = String(prompt("Ciao, Inserisci una parola. Verificherò se  è palindroma"));
+
+//Creo una funzione per capire se la parola inserita è palindroma
+
+//Converto la parola in una sequenza di lettere che comporranno un array
+
+//trasformo il mio array in una nuova parola (stringa)
+
+//Se le due stringhe sono uguali, allora la parola inserita dall'utente sarà palindroma
+
+
+
