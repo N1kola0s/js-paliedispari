a8c11fec3f200ce3fb8b0f9b004e94b47445c837
NicolaMigliore <auster.mig@gmail.com>
2022-04-01 16:02:25 +0200

    add code in palindroma js

--- palindroma/main.js
+++ palindroma/main.js
@@ -6,15 +6,60 @@ Il palindromo è una sequenza di lettere o di sillabe che possa essere letta anc
 
 
 //Chiedo all'untente di inserire una parola
-let user_word = String(prompt("Ciao, Inserisci una parola. Verificherò se  è palindroma"));
+let user_word = String(prompt("Ciao, Inserisci una parola. Verificherò se è palindroma"));
+/* console.log(user_word) */
+/* console.log(typeof user_word) */
 
 //Creo una funzione per capire se la parola inserita è palindroma
 
+function palindrome_word (){
 //Converto la parola in una sequenza di lettere che comporranno un array
 
-//trasformo il mio array in una nuova parola (stringa)
+let array_word = user_word.split("");
+/* console.log(array_word) */
+
+//inverto l'ordine del mio array
+
+let array_word_reverse = array_word.reverse();
+/* console.log(array_word_reverse); */
+
+//trasformo il mio array in una nuova parola (stringa) senza che gli elementi siano separati con le virgole
+
+let new_word = array_word_reverse.join("");
+/* console.log(new_word) */
 
 //Se le due stringhe sono uguali, allora la parola inserita dall'utente sarà palindroma
+
+//imposto la condizione con IF
+
+if (user_word === new_word){
+
+    console.log(`La parola ${user_word} è palindroma`);
+
+} else {
+
+    console.log(`La parola ${user_word} non è palindroma`);
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
